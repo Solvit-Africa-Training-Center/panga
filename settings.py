a4426ea8e0117bde416259d249fde624217a3e1a
mambatheo <muthegene61@gmail.com>
2025-12-25 22:28:32 +0200

    Check on Property/models,views,urls,forms and HTML files under templates folder

--- settings.py
+++ settings.py
@@ -57,8 +57,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'panga.urls'
-#LOGIN_URL = 'login'
-#LOGIN_REDIRECT_URL = 'home'
 
 TEMPLATES = [
     {
@@ -125,17 +123,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 VERIFICATION_CODE_LIFETIME = timedelta(minutes=5)
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'feedyopc@gmail.com'
+EMAIL_HOST_USER = "mutheogene61@gmail.com"
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = 'feedyopc@gmail.com'
+
+
+
